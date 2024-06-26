84c6cb8805dc54b5b2dec1b2471234c1356631a1
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2024-06-26 10:02:02 +0200

    auth home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,39 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <Image
+          key={image.id}
+          src={image.url}
+          width={250}
+          height={250}
+          alt={image.name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <Image
-            key={image.id}
-            src={image.url}
-            width={250}
-            height={250}
-            alt={image.name}
-          />
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In to view images
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
